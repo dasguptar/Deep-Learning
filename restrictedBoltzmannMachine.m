c997c60981de275eb5fa10245b3a5c038a85bca9
Riddhiman Dasgupta <subhoman_sp07@yahoo.co.in>
2014-03-21 13:42:45 +0530

    MATLAB class for RBM

--- restrictedBoltzmannMachine.m
+++ restrictedBoltzmannMachine.m
@@ -1,7 +1,7 @@
 classdef restrictedBoltzmannMachine
     %restrictedBoltzmannMachine Class to implement restricted Boltzmann
     %machines
-    %   Currently simple implementation of binary RBMs whicha re to be
+    %   Currently simple implementation of binary RBMs which are to be
     %   trained using CD-1. To be later generalised/extended to CRBMs and
     %   CD-k. If possible in future add GPU support.
     
@@ -37,9 +37,19 @@ classdef restrictedBoltzmannMachine
     methods
         
         function self = restrictedBoltzmannMachine(numVisible,numHidden,numEpochs)
+        %Class constructor for restrictedBoltzmannMachine class
+        %INPUT:
+        %   numVisible - number of visible units
+        %   numHidden - number of hidden units
+        %   numEpochs - number of epochs for training
+        %OUTPUT:
+        %   self - restrictedBoltzmannMachine class object
+
             self.numVisible = numVisible;
             self.numHidden = numHidden;
             self.numEpochs = numEpochs;
+
+            %Initialise weights and biases randomly
             self.weights = 0.1*randn(self.numVisible,self.numHidden);
             self.dWeights = zeros(self.numVisible,self.numHidden);
             self.visibleBiases = 0.01*randn(1,numVisible);
@@ -49,10 +59,23 @@ classdef restrictedBoltzmannMachine
         end
         
         function probabilities = sigmoid(self,activations)
+        %sigmoid activation function
+        %INPUT:
+        %   activations - data to be activated
+        %OUTPUT:
+        %   probabilities - activation probabilities according to sigmoid function
+
             probabilities = 1./(1+exp(-activations));
         end
         
         function self = train(self,data)
+        %training function for RBM
+        %INPUT:
+        %   data - daa matrix
+        %OUtPUT:
+        %   self - trained restrictedBoltzmannMachine class object
+        
+            %Initislise class members and preallocate space
             self.data = data;
             self.numSamples = size(self.data,1);
             self.visibleActivations = zeros(self.numSamples,self.numHidden);
@@ -64,11 +87,15 @@ classdef restrictedBoltzmannMachine
             self.positiveAssociations = zeros(self.numVisible,self.numHidden);
             self.negativeAssociations = zeros(self.numVisible,self.numHidden);
             
+            %Ierate for specified epochs
             for epoch=1:self.numEpochs
+                %Negative phase - sample hidden from visible
                 self = self.sampleHidden();
                 self.positiveAssociations = self.data' * self.hiddenProbs;
                 self.hiddenProbsPos = self.hiddenProbs;
+                %Contrastive divergence step
                 for k = 1:1:self.cdk
+                    %Positive phase - sample visible from hidden
                     self.phase = true;
                     self = self.sampleVisible();
                     self = self.sampleHidden();
@@ -76,13 +103,17 @@ classdef restrictedBoltzmannMachine
                 self.phase = false;
                 self.negativeAssociations = self.visibleProbs' * self.hiddenProbs;
                 self.hiddenProbsNeg = self.hiddenProbs;
+                %Update weights and biases to learn parameters
                 self = self.updateWeightsAndBiases();
+                %Display current progress
                 fprintf('Epoch: %d | Error = %.10f \n', epoch, self.error);
+                %Show weights in tiled grid of images
                 tiledWeights = showWeights(self,[10 10],[28 28]);
                 imshow(tiledWeights, []), title('Weights');
                 pause(0.1);
             end
             
+            %Clear unused members
             self.visibleActivations = [];   self.hiddenActivations = [];
             self.hiddenProbs = [];          self.visibleProbs= [];
             self.hiddenStates= [];          self.visibleStates = [];
@@ -92,18 +123,25 @@ classdef restrictedBoltzmannMachine
         end
         
         function self = sampleHidden(self)
+        %function to sample hidden units given visible units
+
+            %Negative phase
             if ~self.phase
                 hiddenActivations = self.data * self.weights;
                 self.hiddenActivations = bsxfun(@plus,hiddenActivations,self.hiddenBiases);
+            %Positive phase
             else
                 hiddenActivations = self.visibleProbs * self.weights;
                 self.hiddenActivations=bsxfun(@plus,hiddenActivations,self.hiddenBiases);
             end
+            %Set hidden probabilities and states
             self.hiddenProbs = self.sigmoid(self.hiddenActivations);
             self.hiddenStates = self.hiddenProbs > rand(size(self.hiddenProbs));
         end
         
         function self = sampleVisible(self)
+        %function to sample hidden units given visible units
+
             visibleActivations = self.hiddenStates * transpose(self.weights);
             self.visibleActivations = bsxfun(@plus,visibleActivations,self.visibleBiases);
             self.visibleProbs = self.sigmoid(self.visibleActivations);
@@ -111,17 +149,33 @@ classdef restrictedBoltzmannMachine
         end
         
         function self = updateWeightsAndBiases(self)
+        %function to update weights and biases to learn parameters
+
+            %update weights
             self.dWeights = self.learningRate * (self.positiveAssociations-self.negativeAssociations);
             self.weights = self.weights + self.dWeights;
+
+            %calculate current error
             self.error = sqrt(sum(mean((self.data-self.visibleProbs).^2)));
             
+            %update visible biases
             self.dVisibleBiases = self.learningRate * mean(self.data - self.visibleProbs);
             self.visibleBiases = self.visibleBiases + self.dVisibleBiases;
+            
+            %update hidden biases
             self.dHiddenBiases = self.learningRate * mean(self.hiddenProbsPos - self.hiddenProbsNeg);
             self.hiddenBiases = self.hiddenBiases + self.dHiddenBiases;
         end
         
         function tiledWeightImage = showWeights(self, weightGridSize, weightImageShape)
+        %function to display training progress by tiling weights in a grid
+        %INPUT:
+        %   weightGridSize - size of grid of weights
+        %   weightImageShape - shape of subimage in grid
+        %OUTPUT
+        %   tiledWeightImage - tiled images of weights arranged in a grid
+
+            %Initialise image
             tiledWeightImage = zeros(weightGridSize(1)*weightImageShape(1) + weightGridSize(1) + 1,weightGridSize(2)*weightImageShape(2) + weightGridSize(2) + 1);
             for i = 1:weightGridSize(1)
                 for j = 1:weightGridSize(2)
@@ -137,9 +191,9 @@ classdef restrictedBoltzmannMachine
                     tiledWeightImage(rows:rowe, cols:cole) = weightImage;
                 end
             end
-%             tiledWeightImage = (tiledWeightImage - min(tiledWeightImage(:)))/(max(tiledWeightImage(:)) - min(tiledWeightImage(:)));
-%             tiledWeightImage(1:weightImageShape(1)+1:end, :) = 1;
-%             tiledWeightImage(:, 1:weightImageShape(2)+1:end) = 1;
+            tiledWeightImage = (tiledWeightImage - min(tiledWeightImage(:)))/(max(tiledWeightImage(:)) - min(tiledWeightImage(:)));
+            tiledWeightImage(1:weightImageShape(1)+1:end, :) = 1;
+            tiledWeightImage(:, 1:weightImageShape(2)+1:end) = 1;
         end
         
         function print(self)
